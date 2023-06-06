4e4492ae9ceef8a822aad30d1472ce2123225a60
vova045 <vovatsar@bk.ru>
2023-06-06 19:13:15 +0300

    commit backend

--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -18,9 +18,12 @@ const userSchema = mongoose.Schema({
 }, {
     timestamps: true
 });
+userSchema.methods.mathPassword = async function (enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+}
 
 userSchema.pre('save', async function (next){
-    if (!this, isModified('password')) {
+    if (!this.isModified('password')) {
         next()
     }
 
@@ -28,9 +31,6 @@ userSchema.pre('save', async function (next){
     this.password = await bcrypt.hash(this.password, salt);
 })
 
-userSchema.methods.mathPassword = async function (enteredPassword){
-    return await bcrypt.compare(enteredPassword, this.password());
-}
 
 const User = mongoose.model('User', userSchema);
 
